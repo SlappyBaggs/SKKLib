bc8276e5f298a653a6f8b859ce91245fb16f22c8
SlappyBaggs <85729164+SlappyBaggs@users.noreply.github.com>
2022-10-28 00:43:22 -0400

    rin

--- DB/Elements/Column.cs
+++ DB/Elements/Column.cs
@@ -60,6 +60,7 @@ namespace SKKLib.DB.Elements
             string ret = $"{ColumnNameSQL} ";
             switch (DataType)
             {
+                case "BYTE":
                 case "COUNTER":
                     ret += "INTEGER";
                     break;
@@ -67,7 +68,7 @@ namespace SKKLib.DB.Elements
                     ret += "MEDIUMTEXT";
                     break;
                 case "LONGBINARY":
-                    ret += "BLOB";
+                    ret += "MEDIUMBLOB";
                     break;
                 default:
                     ret += DataType;
@@ -76,6 +77,7 @@ namespace SKKLib.DB.Elements
             
             switch (DataType)
             {
+                case "BYTE":
                 case "CHAR":
                 case "VARCHAR":
                 case "BINARY":
