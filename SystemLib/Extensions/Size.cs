944f18d5d881d60660ffc2c9efa6c92a24f660f4
SlappyBaggs <85729164+SlappyBaggs@users.noreply.github.com>
2022-12-13 06:00:09 -0500

    skibbypoo

---
+++ SystemLib/Extensions/Size.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKKLib.SystemLib.Extensions
+{
+    public static class Extensions
+    {
+        // Size
+        public static Size Multiply(this Size size, double scale) => new Size((int)(size.Width * scale), (int)(size.Height * scale));
+    }
+}
